e8371dd9bfb655e87360574c0de4affbdadc8256
trieuvi2032 <trieuvi3200@gmail.com>
2023-05-04 18:01:48 -0700

    first

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+import 'tachyons';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+reportWebVitals();
